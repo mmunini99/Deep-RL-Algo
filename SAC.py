06a14cbd9621ecf343d2493c54d4b1e203ffef98
mmunini99 <149621935+mmunini99@users.noreply.github.com>
2024-11-26 10:11:11 +0100

    SAC and update the NN

---
+++ SAC.py
@@ -0,0 +1,248 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+from itertools import count
+from collections import namedtuple
+import math
+import random
+import matplotlib
+import matplotlib.pyplot as plt
+from IPython import display
+import numpy as np
+
+from function_tools.Utilities import ReplayMemory, preprocessing_input_state
+from function_tools.Environment import CreateEnvironmentContinuous
+from function_tools.NNetworks import POLICY__SAC, Q__SAC
+
+
+
+class SAC_Agent():
+
+    def __init__(self, ENV_NAME, BATCH_SIZE, GAMMA, ENTROPY_PARAM, K_EPOCHS, STEPS_UPDATE, TAU, LR, REPETITION, N_EPISODES):
+        # hyperparameters
+        self.env_name = ENV_NAME
+        self.batch_size = BATCH_SIZE
+        self.gamma = GAMMA
+        self.entropy_param = ENTROPY_PARAM
+        self.k_epochs = K_EPOCHS
+        self.steps_update = STEPS_UPDATE
+        self.tau = TAU
+        self.lr = LR
+        self.repetition = REPETITION
+        self.num_episodes = N_EPISODES
+        # setting possible accelerator
+        self.device = torch.device(
+                                    "cuda" if torch.cuda.is_available() else
+                                    "cpu"
+                                  )
+        # build the environemnt for simulations
+        self.env = CreateEnvironmentContinuous(self.env_name, self.repetition)
+        # get info about the environment
+        self.dim_actions = self.env.action_space.shape[0]
+        self.action_minimum = torch.from_numpy(self.env.action_space.low).to(self.device)
+        self.action_maximum = torch.from_numpy(self.env.action_space.high).to(self.device)
+        # define the policy and ist target NN
+        self.policy_net = POLICY__SAC(self.dim_actions, self.action_maximum).to(self.device)
+        self.target_policy_net = POLICY__SAC(self.dim_actions, self.action_maximum).to(self.device)
+        self.Q1_net = Q__SAC(self.dim_actions, self.device).to(self.device)
+        self.Q2_net = Q__SAC(self.dim_actions, self.device).to(self.device)
+        self.target_Q1 = Q__SAC(self.dim_actions, self.device).to(self.device)
+        self.target_Q2 = Q__SAC(self.dim_actions, self.device).to(self.device)
+        self.target_policy_net.load_state_dict(self.policy_net.state_dict())
+        self.target_Q1.load_state_dict(self.Q1_net.state_dict())
+        self.target_Q2.load_state_dict(self.Q2_net.state_dict())
+
+        self.optimizer_Q1 = optim.AdamW(self.Q1_net.parameters(), lr=LR, amsgrad=True)
+        self.optimizer_Q2 = optim.AdamW(self.Q2_net.parameters(), lr=LR, amsgrad=True)
+        self.optimizer_policy = optim.AdamW(self.policy_net.parameters(), lr=LR, amsgrad=True)
+        # build the rollout memory
+        self.tuple_store = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
+        self.memory = ReplayMemory(10000, self.tuple_store)
+        # general counter
+        self.steps_done = 0
+        self.epsiode_durations = []
+        self.epsiode_rewards = []
+
+    def select_action(self, state):
+        self.steps_done += 1
+        with torch.no_grad():
+
+            action, _ = self.policy_net(state)
+
+            return action 
+
+    def optimize_model(self):
+
+        if len(self.memory) < self.batch_size: # if not enough data for a training, stop the optimize process
+            return
+
+        transitions = self.memory.sample(self.batch_size) # sample data from the rollour store
+
+        batch = self.tuple_store(*zip(*transitions)) # Transpose operation to make it easier to work with individual compnents rather than tuple like : (state, action, reward, next_state, done)
+
+        non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)), device=self.device, dtype=torch.bool) # Boolean mask for mapping where the terminal states are
+        non_final_next_states = torch.cat([s for s in batch.next_state if s is not None]) # prepare the array for state for Q-learning update rule, without the positions of terminal states
+                                                    
+        state_batch = torch.cat(batch.state)
+        state_batch = state_batch.to(self.device)
+        action_batch = torch.cat(batch.action)
+        action_batch = action_batch.to(self.device)
+        reward_batch = torch.cat(batch.reward)
+        reward_batch = reward_batch.to(self.device)
+
+        ### Q - FUNCTION LOSS
+
+        state_action_values1 = self.Q1_net(state_batch, action_batch)
+        state_action_values2 = self.Q2_net(state_batch, action_batch)
+
+        next_state_values = torch.zeros(self.batch_size, device=self.device)
+        log_action_values= torch.zeros(self.batch_size, device=self.device)
+        with torch.no_grad():
+            future_action_target, log_prob_action_target = self.target_policy_net(non_final_next_states)
+            min_target_Q = torch.min(
+                self.target_Q1(non_final_next_states, future_action_target),
+                self.target_Q2(non_final_next_states, future_action_target)
+            )
+
+
+
+
+
+
+            next_state_values[non_final_mask] = min_target_Q.squeeze(1)
+            log_action_values[non_final_mask] = log_prob_action_target.squeeze(1)
+        # Compute the expected Q values
+        expected_state_action_values = (next_state_values - self.entropy_param*log_action_values)*self.gamma + reward_batch
+
+        # Compute MSE loss
+        criterion = nn.MSELoss()
+        loss_Q1 = criterion(state_action_values1, expected_state_action_values.unsqueeze(1))
+        loss_Q2 = criterion(state_action_values2, expected_state_action_values.unsqueeze(1))
+
+
+
+        ### POLICY-FUNCTION LOSS
+        action_loss, log_prob_action_loss = self.policy_net(state_batch)
+
+        Q_min_value = torch.min(self.Q1_net(state_batch, action_loss), self.Q2_net(state_batch, action_loss))
+
+        loss_policy = (Q_min_value - self.entropy_param*log_prob_action_loss).mean()
+
+
+        self.optimizer_policy.zero_grad()
+        loss_policy.backward()
+        # In-place gradient clipping
+        torch.nn.utils.clip_grad_value_(self.policy_net.parameters(), 100)
+        self.optimizer_policy.step()
+
+        # Optimize the model
+        self.optimizer_Q1.zero_grad()
+        loss_Q1.backward()
+        # In-place gradient clipping
+        torch.nn.utils.clip_grad_value_(self.Q1_net.parameters(), 100)
+        self.optimizer_Q1.step()
+
+
+        self.optimizer_Q2.zero_grad()
+        loss_Q2.backward()
+        # In-place gradient clipping
+        torch.nn.utils.clip_grad_value_(self.Q2_net.parameters(), 100)
+        self.optimizer_Q2.step()
+
+    def training(self):
+
+        for i_episode in range(self.num_episodes):
+            # reward tracker only for plot porpouse
+            track_rew = 0
+            # Initialize the environment and get its state
+            state, info = self.env.reset()
+            state = preprocessing_input_state(state, self.device) # this allows the correct shape for the CNN
+            for t in count():
+                action = self.select_action(state)
+                observation, reward, terminated, truncated, _ = self.env.step(action.cpu().detach().numpy().squeeze(0)) # transform the output of the NN to a format that is accepted by the environment
+                track_rew += (reward-track_rew)/(t+1)
+                reward = torch.tensor([reward], device=self.device)
+                done = terminated or truncated
+
+                if terminated or truncated:
+                    next_state = None
+                else:
+                    next_state = preprocessing_input_state(observation, self.device) # save directly the correct shape for the next state for CNN, as the previous correction of format won't be repeated
+
+                # Store the transition in memory
+                self.memory.push(state, action, next_state, reward)
+
+                # Move to the next state
+                state = next_state
+
+                # Perform one step of the optimization (on the policy network)
+                if self.steps_done > 0 and self.steps_done % self.steps_update:
+                        # Perform one step of the optimization (on both NN)
+                        for _ in range(self.k_epochs):
+                            self.optimize_model()
+
+                        # Soft update of the target network's weights
+                        # Polyak Average
+                        target_Q1_state_dict = self.target_Q1.state_dict()
+                        target_Q2_state_dict = self.target_Q2.state_dict()
+                        target_policy_net_state_dict = self.target_policy_net.state_dict()
+
+                        Q1_net_state_dict = self.Q1_net.state_dict()
+                        Q2_net_state_dict = self.Q2_net.state_dict()
+                        policy_net_state_dict = self.policy_net.state_dict()
+
+
+                        for keyQ1 in Q1_net_state_dict:
+                            target_Q1_state_dict[keyQ1] = Q1_net_state_dict[keyQ1]*self.tau + target_Q1_state_dict[keyQ1]*(1-self.tau)
+                        self.target_Q1.load_state_dict(target_Q1_state_dict)
+                        for keyQ2 in Q2_net_state_dict:
+                            target_Q2_state_dict[keyQ2] = Q2_net_state_dict[keyQ2]*self.tau + target_Q2_state_dict[keyQ2]*(1-self.tau)
+                        self.target_Q2.load_state_dict(target_Q2_state_dict)
+                        for key in policy_net_state_dict:
+                            target_policy_net_state_dict[key] = policy_net_state_dict[key]*self.tau + target_policy_net_state_dict[key]*(1-self.tau)
+                        self.target_policy_net.load_state_dict(target_policy_net_state_dict)
+
+                
+
+                if done:
+                    self.epsiode_durations.append(i_episode)
+                    self.epsiode_rewards.append(track_rew)
+                    self.plot_episode()
+                    break
+
+
+
+    def plot_episode(self):
+
+        is_ipython = 'inline' in matplotlib.get_backend()         
+
+        plt.ion()
+
+        plt.figure(1)
+
+        plt.clf()
+        plt.title('Training...')  
+
+        plt.xlabel('Episode')
+        plt.ylabel('Mean rewards') 
+
+        array_epis = np.array(self.epsiode_durations)  
+        array_rew = np.array(self.epsiode_rewards)  
+
+        plt.plot(array_epis, array_rew)
+
+        plt.pause(0.001) 
+
+        if is_ipython:
+            display.display(plt.gcf())
+            display.clear_output(wait=True)
+
+
+
+            
+
+
+
+
--- function_tools/NNetworks.py
+++ function_tools/NNetworks.py
@@ -415,3 +415,110 @@ class VALUE__PPO(nn.Module):
         x = self.fc3(x)
 
         return x
+
+
+# SAC
+class POLICY__SAC(nn.Module):
+
+    def __init__(self, dim_action, max_value):
+        super(POLICY__SAC, self).__init__()
+        self.max_value = max_value
+        # Convolutional layers
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=(3, 3), stride=4, padding=2)  
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=(3, 3), stride=2, padding=2) 
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=(3, 3), stride=1, padding=1) 
+
+        # Batch Normalization
+        self.bn1 = nn.BatchNorm2d(32)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.bn3 = nn.BatchNorm2d(64)
+
+        # Fully connected layers
+        self.fc1 = nn.Linear(12544, 2048)  # Flattened size after convolution layers
+        self.fc2 = nn.Linear(2048, 512)
+        self.mean = nn.Linear(512, dim_action)
+        self.sd = nn.Linear(512, dim_action)
+
+    def forward(self, x):
+
+        x = x.clone()
+        # Convolutional block with ReLU and Batch Normalization
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+
+        # Flattening the output for the fully connected layers
+        x = x.view(x.size(0), -1)
+
+        # Fully connected layers with ReLU
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+
+        # Output layer
+        mu_param = self.mean(x)
+        sd_param = self.sd(x)
+        sd_param = F.softplus(sd_param) + 0.0001 #to ensure sd always > 0
+
+        pdf = Normal(mu_param, sd_param)
+        action_sample = pdf.sample()
+        log_probability = pdf.log_prob(action_sample)
+        log_prob_single = log_probability.sum(dim=-1, keepdim = True) #to convert log prob for each action in a single log prob.
+        log_prob_single -= (2*(np.log(2)-action_sample-F.softplus(-2*action_sample))).sum(dim=-1, keepdim = True) #stability of log prob
+        action = torch.tanh(action_sample)*self.max_value
+
+        return action, log_prob_single
+
+
+
+class Q__SAC(nn.Module):
+
+    def __init__(self, dim_action, device):
+        super(Q__SAC, self).__init__()
+        self.device = device
+        self.dim_action = dim_action
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=(3, 3), stride=4, padding=2)  
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=(3, 3), stride=2, padding=2) 
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=(3, 3), stride=1, padding=1) 
+
+        # Batch Normalization
+        self.bn1 = nn.BatchNorm2d(32)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.bn3 = nn.BatchNorm2d(64)
+
+        # Fully connected layers
+        self.fc1 = nn.Linear(12544+self.dim_action, 2048)  # Flattened size after convolution layers
+        self.fc2 = nn.Linear(2048, 512)
+        self.fc3 = nn.Linear(512, 1)
+
+    def forward(self, x, action_array):
+
+        # Convolutional block with ReLU and Batch Normalization
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+
+
+
+
+        # Flattening the output for the fully connected layers
+        x = x.view(x.size(0), -1)
+
+        act = torch.tensor(action_array)
+
+        act = act.to(self.device)
+
+
+        # Include the action
+        x = torch.cat([x, act], dim=-1)
+
+
+
+
+        # Fully connected layers with ReLU
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+
+        # Output layer
+        x = self.fc3(x)
+
+        return x
